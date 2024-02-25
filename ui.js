60357c1c7f5e26dfbaba3c31f66c2a6c2a2eca9c
Silver <rhyskoerber@gmail.com>
2024-02-25 15:39:05 +1030

    e

--- ui.js
+++ ui.js
@@ -18,7 +18,7 @@ class UI {
     setup() {
         let viewportMeta = document.createElement("meta")
         viewportMeta.name = "viewport"
-        viewportMeta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover"
+        viewportMeta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         document.head.appendChild(viewportMeta)
         document.body.style.overflow = "hidden"
         this.page = document.getElementById("page")
@@ -380,6 +380,7 @@ class UI {
                 this.element.style.display = "none"
                 this.element.addEventListener("touchstart", (event) => {
                     event.preventDefault()
+                    this.element.blur()
                     this.element.style.top = "0px"
                     this.element.focus()
                 }, {passive: false})
